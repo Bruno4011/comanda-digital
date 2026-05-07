b7d2d8d335eea99776523c7c6aae1732b71747b4
Bruno Ramos Castellar <143856350+Kakay01@users.noreply.github.com>
2026-05-07 20:24:59 -0300

    Update CategoriaRepository.java

--- backend/src/main/java/com/comanda/repository/CategoriaRepository.java
+++ backend/src/main/java/com/comanda/repository/CategoriaRepository.java
@@ -1,4 +1,7 @@
 package com.comanda.repository;
 import com.comanda.entity.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {}
+import java.util.Optional;
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+    Optional<Categoria> findByNome(String nome);
+}
