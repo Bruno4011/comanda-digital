f1692ed465467b0bb1501f3274b8c429a85ca814
Bruno Ramos Castellar <143856350+Kakay01@users.noreply.github.com>
2026-05-20 21:50:48 -0300

    Update admin.component.ts

--- frontend/src/app/features/admin/admin.component.ts
+++ frontend/src/app/features/admin/admin.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../core/services/auth.service';
 
@@ -8,10 +8,7 @@ import { AuthService } from '../../core/services/auth.service';
   imports: [CommonModule],
   templateUrl: './admin.component.html'
 })
-export class AdminComponent implements OnInit {
+export class AdminComponent {
   user = this.auth.getUser();
-
   constructor(private auth: AuthService) {}
-
-  ngOnInit() {}
 }
